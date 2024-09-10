2ecfecf3a7a2bfeef5cd92597edab6862da80a63
Taras Samoilenko <tsamoilenko97@gmail.com>
2024-09-10 09:58:02 +0300

    fetcher

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,11 +2,9 @@ async function loadApi<T>(url: string): Promise<T> {
   try {
     const res = await fetch(url);
     const data = await res.json();
-    console.log('🚀 ~ res==>>', data);
     return data;
   } catch (e) {
-    console.log('🚀 ~ loadApi ~ e==>>', e);
-    return null;
+    return e;
   }
 }
 
